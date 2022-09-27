10e728314ad62e91c4080b8a3c9d72b78763b033
Ryu0012 <93696435+Ryu0012@users.noreply.github.com>
2022-09-27 15:16:17 +0900

    복습

--- app.js
+++ app.js
@@ -1,80 +1,97 @@
+const eraserBtn = document.getElementById("eraser");
+const clearAllBtn = document.getElementById("clear-all");
+const colorBtn = document.getElementById("color");
+const colorOption = Array.from(document.getElementsByClassName("color"));
 const modeBtn = document.getElementById("mode-btn");
-
-const colorOptions = Array.from(
-  document.getElementsByClassName("color-option")
-);
-const lineWidth = document.getElementById("line-width");
-const colorBtn = document.getElementById("color-btn");
-
-const canvas = document.querySelector("canvas");
+const lineWidth = document.getElementById("range-width");
+const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const CANVAS_WIDTH = 1000;
 const CANVAS_HEIGHT = 800;
+const CANVAS_WIDTH = 800;
 
 canvas.height = CANVAS_HEIGHT;
 canvas.width = CANVAS_WIDTH;
 let isPainting = false;
-let isFilling = false;
+let isFill = false;
+ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 
-colors = ["#FFC312", "#F79F1F", "#EE5A24", "#EA2027", "#C4E538"];
+function onMouseMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.beginPath();
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
 
 function onMouseDown() {
   isPainting = true;
   ctx.beginPath();
 }
 
-function onMove(event) {
-  if (isPainting) {
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
-    return;
-  }
-  ctx.moveTo(event.offsetX, event.offsetY);
-}
-
-function cancelPainting() {
+function onMouseUp() {
   isPainting = false;
 }
 
-function onlineWidth(event) {
+function onLineWidth(event) {
   ctx.lineWidth = event.target.value;
 }
 
-function onChangeColor(event) {
-  ctx.strokeStyle = event.target.value;
-  ctx.fillStyle = event.target.value;
+function onModeBtn() {
+  if (isFill) {
+    isFill = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFill = true;
+    modeBtn.innerText = "Stroke";
+  }
 }
 
-function onColorClick(event) {
-  ctx.strokeStyle = event.target.dataset.color;
-  ctx.fillStyle = event.target.dataset.color;
-  colorBtn.value = event.target.dataset.color;
-}
-
-function onColorFillClick() {
-  if (isFilling) {
+function onFillPaint() {
+  if (isFill) {
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   }
 }
 
-function onModeClick() {
-  if (isFilling) {
-    isFilling = false;
-    modeBtn.innerText = "fill";
-  } else {
-    isFilling = true;
-    modeBtn.innerText = "draw";
-  }
+function colorStyle(colorValue) {
+  ctx.fillStyle = colorValue;
+  ctx.strokeStyle = colorValue;
 }
 
-canvas.addEventListener("mousedown", onMouseDown);
-canvas.addEventListener("mousemove", onMove);
-canvas.addEventListener("mouseup", cancelPainting);
-canvas.addEventListener("mouseleave", cancelPainting);
-canvas.addEventListener("click", onColorFillClick);
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  colorStyle(colorValue);
+  colorBtn.value = colorValue;
+}
 
-lineWidth.addEventListener("change", onlineWidth);
-colorBtn.addEventListener("change", onChangeColor);
-colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
-modeBtn.addEventListener("click", onModeClick);
+function onColorChange(event) {
+  const colorValue = event.target.value;
+  colorStyle(colorValue);
+}
+
+function onClearAll() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = "white";
+  isFill = false;
+  modeBtn.innerText = "Fill";
+}
+
+canvas.addEventListener("mousemove", onMouseMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("mouseleave", onMouseUp);
+lineWidth.addEventListener("change", onLineWidth);
+canvas.addEventListener("click", onFillPaint);
+colorOption.forEach((color) => addEventListener("click", onColorClick));
+colorBtn.addEventListener("change", onColorChange);
+
+modeBtn.addEventListener("click", onModeBtn);
+clearAllBtn.addEventListener("click", onClearAll);
+eraserBtn.addEventListener("click", onEraserClick);
